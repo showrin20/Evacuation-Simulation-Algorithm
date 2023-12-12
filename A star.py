5addd22c317ad3a5be5e814deba841a95b1c5ef3
showrin20 <showrin.rahman@g.bracu.ac.bd>
2023-12-12 19:55:54 +0600

    first commit

--- A star.py
+++ A star.py
@@ -2,80 +2,99 @@ import pygame
 import random
 import heapq
 import math
-
-# Define simulation parameters
-grid_size = 20  # Number of grid cells in each dimension
-cell_size = 30  # Size of each grid cell in pixels
-population_density = 0.2  # Percentage of population density
-evacuation_speed = 100  # Speed at which people evacuate (lower is faster)
-cyclone_path = [(0, 0), (5, 10), (10, 15), (15, 19), (19, 19)]  # Example cyclone path
+import sys
 
 # Initialize pygame
 pygame.init()
+grid_size = 20
+cell_size = 30
 screen = pygame.display.set_mode((grid_size * cell_size, grid_size * cell_size))
 pygame.display.set_caption("Cyclone Evacuation Simulation")
 
-# Define grid representing the map
+# Load background image
+background = pygame.image.load('background.png')
+background = pygame.transform.scale(background, (grid_size * cell_size, grid_size * cell_size))
+
+# Define grid and other parameters
 grid = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
+population_density = 0.2
+frames_per_second = 1  # Adjusted to slow down the simulation
 
-# Define colors
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
+# Colors and cell states
+WHITE, BLUE, RED, GREEN = (255, 255, 255), (0, 0, 255), (255, 0, 0), (0, 255, 0)
+EMPTY, RESIDENT, CYCLONE, SHELTER = 0, 1, 2, 2
 
-# Define cell states
-EMPTY = 0
-RESIDENT = 1
-CYCLONE = 2
-SHELTER = 3
-
-# Define class for residents
+# Resident class
 class Resident(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((cell_size, cell_size))
-        self.image.fill(BLUE)
+        self.image = pygame.Surface((cell_size, cell_size), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, (255, 0, 0), (cell_size // 2, cell_size // 2), cell_size // 2 - 5)
         self.rect = self.image.get_rect()
         self.rect.x = x * cell_size
         self.rect.y = y * cell_size
+        self.path = None
 
     def update(self):
-        pass
+        if self.path:
+            next_cell = self.path.pop(0)
+            self.rect.x = next_cell[1] * cell_size
+            self.rect.y = next_cell[0] * cell_size
+            if not self.path:
+                self.path = None
 
-# Create groups for sprites
-all_sprites = pygame.sprite.Group()
-residents = pygame.sprite.Group()
-shelters = []
+# Shelter class
+class Shelter(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((cell_size, cell_size))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x * cell_size
+        self.rect.y = y * cell_size
+        self.capacity = 100  # Increased Shelter capacity
+        self.residents = 0  # Number of residents currently in the shelter
 
-# Populate grid with population based on density
+# Sprite groups
+all_sprites, residents, shelters = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
+
+#Populate grid with residents
 for i in range(grid_size):
     for j in range(grid_size):
         if random.random() < population_density:
             resident = Resident(j, i)
-            all_sprites.add(resident)
             residents.add(resident)
+            all_sprites.add(resident)
             grid[i][j] = RESIDENT
 
-# Main simulation loop
-running = True
-clock = pygame.time.Clock()
+# Define shelters with increased capacity
+shelter_locations = [(15, 15)]  # Added extra shelters
+for loc in shelter_locations:
+    shelter = Shelter(*loc)
+    shelters.add(shelter)
+    all_sprites.add(shelter)
+    grid[loc[0]][loc[1]] = SHELTER
 
+# Heuristic function for A*
 def heuristic(node, goal):
     return math.sqrt((node[0] - goal[0]) ** 2 + (node[1] - goal[1]) ** 2)
 
+# A* algorithm
 def astar(grid, start, goal):
     open_list = []
     closed_set = set()
-    heapq.heappush(open_list, (0, start))
     came_from = {}
 
-    g_score = {(node[0], node[1]): float('inf') for node in grid}
+    g_score = {(x, y): float("inf") for x in range(grid_size) for y in range(grid_size)}
+    f_score = {(x, y): float("inf") for x in range(grid_size) for y in range(grid_size)}
     g_score[start] = 0
-    f_score = {(node[0], node[1]): float('inf') for node in grid}
     f_score[start] = heuristic(start, goal)
+
+    heapq.heappush(open_list, (f_score[start], start))
 
     while open_list:
         current = heapq.heappop(open_list)[1]
+
         if current == goal:
             path = []
             while current in came_from:
@@ -85,88 +104,96 @@ def astar(grid, start, goal):
             return path
 
         closed_set.add(current)
-        for neighbor in get_neighbors(current):
+        for neighbor in get_neighbors(grid, current):
             if neighbor in closed_set:
                 continue
 
-            tentative_g_score = g_score[(current[0], current[1])] + 1
+            tentative_g_score = g_score[current] + 1
 
-            if neighbor not in [i[1] for i in open_list]:
-                heapq.heappush(open_list, (tentative_g_score + heuristic(neighbor, goal), neighbor))
-            elif tentative_g_score >= g_score[(neighbor[0], neighbor[1])]:
-                continue
+            if tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goal)
 
-            came_from[neighbor] = current
-            g_score[(neighbor[0], neighbor[1])] = tentative_g_score
-            f_score[(neighbor[0], neighbor[1])] = g_score[(neighbor[0], neighbor[1])] + heuristic(neighbor, goal)
+                if neighbor not in [i[1] for i in open_list]:
+                    heapq.heappush(open_list, (f_score[neighbor], neighbor))
 
     return None
 
-def get_neighbors(node):
+# Neighbors function
+def get_neighbors(grid, node):
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     neighbors = []
-    x, y = node
-    if x > 0:
-        neighbors.append((x - 1, y))
-    if x < grid_size - 1:
-        neighbors.append((x + 1, y))
-    if y > 0:
-        neighbors.append((x, y - 1))
-    if y < grid_size - 1:
-        neighbors.append((x, y + 1))
+    for dx, dy in directions:
+        x, y = node[0] + dx, node[1] + dy
+        if 0 <= x < grid_size and 0 <= y < grid_size:
+            neighbors.append((x, y))
     return neighbors
 
-def find_nearest_shelter(resident, shelters):
-    if not shelters:
+# Find nearest shelter
+def find_nearest_shelter_a_star(resident):
+    nearest_shelter = None
+    min_distance = float("inf")
+    resident_pos = (resident.rect.y // cell_size, resident.rect.x // cell_size)
+
+    for shelter in shelters:
+        if shelter.capacity > shelter.residents:
+            shelter_pos = (shelter.rect.y // cell_size, shelter.rect.x // cell_size)
+            path = astar(grid, resident_pos, shelter_pos)
+
+            if path and len(path) < min_distance:
+                nearest_shelter = shelter
+                min_distance = len(path)
+
+    if nearest_shelter:
+        nearest_shelter.residents += 1
+        return path
+    else:
         return None
 
-    resident_position = (resident.rect.y // cell_size, resident.rect.x // cell_size)
-    nearest_shelter = min(shelters, key=lambda shelter: len(astar(grid, resident_position, shelter)))
-    return nearest_shelter
 
-# Define shelter locations
-shelters = [(5, 5), (15, 5)]  # Example shelter locations
+# Cyclone path simulation
+cyclone_path = [(0, 0)]
+def update_cyclone_path():
+    last_pos = cyclone_path[-1]
+    next_pos = (last_pos[0] + random.choice([-1, 0, 1]), last_pos[1] + random.choice([-1, 0, 1]))
+    if 0 <= next_pos[0] < grid_size and 0 <= next_pos[1] < grid_size:
+        cyclone_path.append(next_pos)
 
+# Main simulation loop
+running = True
+clock = pygame.time.Clock()
 while running:
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                running = False
 
-    # Simulate cyclone progression
-    if len(cyclone_path) > 0:
-        cyclone_position = cyclone_path.pop(0)
-        grid[cyclone_position[0]][cyclone_position[1]] = CYCLONE
+    update_cyclone_path()
 
-    # Evacuation simulation
     for resident in residents:
-        nearest_shelter = find_nearest_shelter(resident, shelters)
-        if nearest_shelter:
-            path = astar(grid, (resident.rect.y // cell_size, resident.rect.x // cell_size), nearest_shelter)
+        if not resident.path:
+            path = find_nearest_shelter_a_star(resident)
             if path:
-                for cell in path:
-                    pygame.draw.rect(screen, WHITE, (cell[1] * cell_size, cell[0] * cell_size, cell_size, cell_size))
-                    pygame.display.update()
-                    pygame.time.wait(evacuation_speed)
-                shelter_x, shelter_y = nearest_shelter
-                resident.rect.x = shelter_y * cell_size
-                resident.rect.y = shelter_x * cell_size
-
-    # Clear the screen
-    screen.fill((255, 255, 255))
+                resident.path = path
+        resident.update()
 
     # Draw the grid
     for i in range(grid_size):
         for j in range(grid_size):
-            if grid[i][j] == RESIDENT:
-                pygame.draw.rect(screen, BLUE, (j * cell_size, i * cell_size, cell_size, cell_size))
-            elif grid[i][j] == CYCLONE:
+            #if grid[i][j] == RESIDENT:
+                #pygame.draw.rect(screen, BLUE, (j * cell_size, i * cell_size, cell_size, cell_size))
+            if grid[i][j] == CYCLONE:
                 pygame.draw.rect(screen, RED, (j * cell_size, i * cell_size, cell_size, cell_size))
             elif grid[i][j] == SHELTER:
-                pygame.draw.rect(screen, (0, 255, 0), (j * cell_size, i * cell_size, cell_size, cell_size))
+                pygame.draw.rect(screen, GREEN, (j * cell_size, i * cell_size, cell_size, cell_size))
+
+    # Update and draw sprites
+    all_sprites.update()
+    all_sprites.draw(screen)
 
     pygame.display.flip()
-    clock.tick(5)  # Adjust the simulation speed here
+    clock.tick(frames_per_second)
 
 pygame.quit()
+sys.exit()
